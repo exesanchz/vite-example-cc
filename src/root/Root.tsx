172e3378157a8fc5fe2938fb25483eaa9d9c493b
exesanchz <exequiel_2080@hotmail.com>
2023-11-19 23:45:59 +0100

    dots undo/redo functions

--- src/root/Root.tsx
+++ src/root/Root.tsx
@@ -3,18 +3,41 @@ import { IDot } from "../interfaces/Dot";
 
 function Root() {
   const [dots, setDots] = useState<IDot[]>([]);
+  const [storageDots, setStorageDots] = useState<IDot[]>([]);
 
   const draw = (e: MouseEvent) => {
     const { clientX, clientY } = e;
     setDots([...dots, { x: clientX, y: clientY }]);
-    console.log(e);
+  };
+
+  const undo = () => {
+    if (dots.length > 0) {
+      const newDots = [...dots];
+      const lastDot = newDots.pop() as IDot;
+      Promise.all([
+        setStorageDots([...storageDots, lastDot]),
+        setDots(newDots),
+      ]);
+    }
+  };
+
+  const redo = () => {
+    if (storageDots.length > 0) {
+      const newStorage = [...storageDots];
+      const redoDot = newStorage.pop() as IDot;
+      Promise.all([setStorageDots(newStorage), setDots([...dots, redoDot])]);
+    }
   };
 
   return (
     <>
       <div id="button-wrapper">
-        <button className="button-primary">Undo</button>
-        <button className="button-primary">Redo</button>
+        <button className="button-primary" onClick={undo}>
+          Undo
+        </button>
+        <button className="button-primary" onClick={redo}>
+          Redo
+        </button>
       </div>
       <div id="click-area" onClick={draw}>
         {dots.map(({ x, y }: IDot, i: number) => (
